348268ba86da187f75363487a91d74c9f472ddaf
alissongoulart <alisson.av@outlook.com>
2024-05-16 21:16:59 -0300

    Incresing velocity of mercury when user clicks on sun

--- Sistema Solar/main.js
+++ Sistema Solar/main.js
@@ -1,52 +1,18 @@
-// Selecione o elemento .sol
-const solElement = document.querySelector('.sol');
+let mercurySpeed = 4.3
 
-// Inicialize uma variável para rastrear o número de cliques
-let clickCount = 0;
+const mercury = document.getElementById('mercurio')
+const sol = document.getElementById('sol')
 
-// Adicione um ouvinte de evento de clique ao elemento .sol
-solElement.addEventListener('click', () => {
-    // Incrementar o número de cliques
-    clickCount++;
+window.onload = () => {
+    updateMercurySpeed(mercurySpeed)
+}
 
-    // Se o número de cliques for ímpar (1ª, 3ª, 5ª...), duplique a velocidade da animação
-    // Se o número de cliques for par (2ª, 4ª, 6ª...), mantenha a velocidade atual da animação
-    switch (clickCount) {
-        case 1:
-            multiplyAnimationSpeed(2);
-            break;
-        case 3:
-            multiplyAnimationSpeed(5);
-            break;
-        case 5:
-            multiplyAnimationSpeed(10);
-            break;
-        case 7:
-            // Retorne à velocidade original da animação após o quarto clique
-            multiplyAnimationSpeed(1);
-            // Resetar o contador de cliques
-            clickCount = 0;
-            break;
-    }
-});
+sol.addEventListener('click', () => {
+    updateMercurySpeed(mercurySpeed / 2)
+})
 
-// Função para multiplicar a velocidade da animação
-function multiplyAnimationSpeed(factor) {
-    // Selecione todos os elementos dentro de .container que possuem animação
-    const animatedElements = document.querySelectorAll('.container > *');
+function updateMercurySpeed(speed) {
+    mercury.style.animation = `orbit ${speed}s linear infinite`
 
-    // Iterar sobre os elementos e atualizar a propriedade de animação
-    animatedElements.forEach(element => {
-        // Obter a duração atual da animação
-        const currentAnimationDuration = window.getComputedStyle(element).animationDuration;
-
-        // Remover 's' do final e converter para número
-        const currentDurationValue = parseFloat(currentAnimationDuration);
-
-        // Multiplicar a duração atual pela fator
-        const newDurationValue = currentDurationValue * factor;
-
-        // Atualizar a propriedade de animação com a nova duração
-        element.style.animationDuration = `${newDurationValue}s`;
-    });
+    mercurySpeed = speed
 }
