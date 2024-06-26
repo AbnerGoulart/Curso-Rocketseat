34462df302cb3075c8a5229d264080f785b899a1
Abner Goulart <abnergoulart.10@gmail.com>
2024-06-26 15:27:15 -0300

    encerramento do SQL e inicio do KNEXJS

--- Desafio 17/src/controller/UsersController.js
+++ Desafio 17/src/controller/UsersController.js
@@ -1,4 +1,4 @@
-const {hash} = require("bcryptjs")
+const {hash, compare} = require("bcryptjs")
 const AppError = require('../utils/AppError')
 
 const sqliteConnection = require("../database/sqlite")
@@ -25,7 +25,7 @@ class UsersController {
   }
 
   async update(request, response){
-    const {name, email} = request.body;
+    const {name, email, password, old_password} = request.body;
     const {id} = request.params;
 
     const database = await sqliteConnection();
@@ -37,20 +37,34 @@ class UsersController {
 
     const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== id){
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
       throw new AppError("Este email já está em uso");
     }
 
-    user.name = name;
-    user.email = email;
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
 
+    if(password && !old_password){
+      throw new AppError("Para atualizar, confirme a senha anterior")
+    }
+
+    if(password && old_password){
+      const checkOldPassword = await compare(old_password, user.password)
+      if (!checkOldPassword) {
+        throw new AppError ("Senha anterior inválida")
+      }
+
+      user.password = await hash(password, 8)
+    }
+    
     await database.run(`
       UPDATE users SET
       name = ?,
       email = ?,
-      updated_at = ?,
+      password = ?,
+      updated_at = DATETIME ('now')
       WHERE id = ?`,
-      [user.name, user.email, new Date(), id]
+      [user.name, user.email, user.password, id]
     );
 
     return response.json();
