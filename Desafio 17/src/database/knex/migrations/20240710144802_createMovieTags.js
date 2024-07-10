987662c6796c5dafce061a3732a03351810be753
Abner Goulart <abnergoulart.10@gmail.com>
2024-07-10 11:56:24 -0300

    cração de tabelas relacionais

--- Desafio 17/src/database/knex/migrations/20240710144802_createMovieTags.js
+++ Desafio 17/src/database/knex/migrations/20240710144802_createMovieTags.js
@@ -1,9 +1,9 @@
 
-exports.up = knex => knex.schema.createTable("notes", table => {
+exports.up = knex => knex.schema.createTable("movie_notes", table => {
   table.increments("id");
   table.text("title");
   table.text("description");
-
+  
   table.integer("rating");
   table.integer("user_id").references("id").inTable("users");
 
