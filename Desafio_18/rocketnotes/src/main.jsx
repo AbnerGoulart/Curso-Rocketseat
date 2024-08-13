545a29475880d70d69227ef0d37e96177d022989
Abner Goulart <abnergoulart.10@gmail.com>
2024-08-12 21:54:23 -0300

    implementação do textarea

--- Desafio_18/rocketnotes/src/main.jsx
+++ Desafio_18/rocketnotes/src/main.jsx
@@ -9,6 +9,7 @@ import {Home} from './pages/Home';
 import { SignIn } from './pages/SignIn';
 import { SignUp } from './pages/SignUp';
 import { Profile } from './pages/Profile';
+import { New } from './pages/New';
 
 
 
@@ -17,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>,
 )
